31728cc7e019634f7ff502f745ee5d78299e8b02
rohan <rohanrv2004@gmail.com>
2026-04-22 09:34:35 +0530

    chunk

---
+++ backend/ingestion/chunker.py
@@ -0,0 +1,11 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+def chunk_documents(documents):
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=50
+    )
+
+    chunks = splitter.split_documents(documents)
+    return chunks
